84cdbb071d499ffd0fc3fbf64bde2c65f084ea1f
Tony Truong <tonytruong0698@gmail.com>
2023-05-08 09:07:01 -0700

    finalized GET products function to handle multiple queries

--- client/src/components/RelatedItems/ProductList.jsx
+++ client/src/components/RelatedItems/ProductList.jsx
@@ -1,7 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Products = () => (
-    <>this is Products</>
-);
+const Products = () => {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    axios({
+      url: '/classes/products',
+      method: 'get',
+      params: {
+        page: 'related',
+        id: 40344,
+      },
+    })
+      .then((res) => { setList(res.data); });
+  }, []);
+  return (
+    <>this is related products</>
+  );
+};
 
 export default Products;
