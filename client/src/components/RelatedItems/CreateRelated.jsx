4b3ce517ce43e4b8b6c5659c2f684ec76ff7b124
Tony Truong <Tonytruong0698@gmail.com>
2023-05-08 14:13:45 -0700

    saving client changes

--- client/src/components/RelatedItems/CreateRelated.jsx
+++ client/src/components/RelatedItems/CreateRelated.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const CreateRelated = () => {
-
+const CreateRelated = ({ id, handler, defaultProduct }) => {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    axios({
+      url: '/classes/productsquery',
+      method: 'get',
+      params: {
+        id: id,
+      },
+    })
+      .then((res) => { setList(res.data); });
+  }, []);
+  console.log(list);
+  return (
+    <div onClick={() => {
+      handler(id);
+    }}>
+      <button onClick={(e) => {
+        e.stopPropagation();
+        console.log('compared');
+      }}>compare</button>
+    <div>Category: {list.category}</div>
+    <div>Product Name: {list.name}</div>
+    <div>Price: {list.default_price}</div>
+    <div>Rating: to be implemented</div>
+    </div>
+  );
 };
 
 export default CreateRelated;
--- client/src/components/RelatedItems/ProductList.jsx
+++ client/src/components/RelatedItems/ProductList.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import CreateRelated from './CreateRelated.jsx';
 
 const Products = () => {
+  const [defaultProduct, setDefault] = useState(40344);
+  const defaultHandler = (e) => {
+    setDefault(e);
+  };
   const [list, setList] = useState([]);
   useEffect(() => {
+    console.log(defaultProduct);
     axios({
-      url: '/classes/products',
+      url: '/classes/productsquery',
       method: 'get',
       params: {
         page: 'related',
-        id: 40344,
+        id: defaultProduct,
       },
     })
       .then((res) => { setList(res.data); });
-  }, []);
+  }, [defaultProduct]);
   return (
-    <>this is related products</>
+    <>
+    {list.map((product, index) => <CreateRelated
+    key={index} id={product} handler={defaultHandler} defaultProduct={defaultProduct}/>)}
+    </>
   );
 };
 
