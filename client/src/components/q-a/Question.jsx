31ea315452e263fc47e7d60ac1f08a35f6071e59
Gabe Jimenez <jimenezgabriel12@gmail.com>
2023-05-09 18:18:23 -0600

    Add structure to Answers component and finished structure/functionality on Questions component

--- client/src/components/q-a/Question.jsx
+++ client/src/components/q-a/Question.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Answer from './Answer.jsx';
 
-const Question = ({ question }) => (
-  <div>
-    <div>Q: {question.question_body}</div>
-    <div>Helpful? {question.question_helpfulness}</div>
-    <button>Add an Answer</button>
-    <Answer />
-  </div>
-);
+const Question = ({ question }) => {
+  const [answers, setAnswers] = useState([]);
+  const [showAnswers, setShowAnswers] = useState([]);
+
+
+
+  return (
+    <div>
+      <div>Q: {question.question_body}</div>
+      <div>Helpful? <button>Yes({question.question_helpfulness})</button></div>
+      <button>Add an Answer</button>
+      <ul>
+          {answers.length ? showAnswers.map((answer, i) => (
+            <li key={i}>
+              <Answer answer={answer}/>
+            </li>
+          )) : <li>Loading Answers...</li>}
+        </ul>
+        {showAnswers.length !== answers.length ? <button>Load More Answers</button> : null}
+    </div>
+  );
+};
 
 export default Question;
