b74ef1503b3b797262444db2180890ebee540c57
Nathaniel Franklin <nmf900@gmail.com>
2023-05-15 04:50:06 -0700

    Add to Cart. Unknown bug.

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,31 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Information from './Information.jsx';
-import StaticStarRating from './StaticStarRating.jsx';
+// import StaticStarRating from './StaticStarRating.jsx';
 import StyleSelector from './StyleSelector.jsx';
+import AddToCart from './AddToCart.jsx';
 
 const Overview = () => {
   const [product, setProduct] = useState(null);
+  const [selectedStyleId, setSelectedStyleId] = useState(null);
 
   useEffect(() => {
     // Replace 40344 with the actual product ID you want to fetch
     axios.get('classes/products/40344')
       .then((response) => {
         setProduct(response.data);
+        // Set the selected style ID to the ID of the first style in the response
+        setSelectedStyleId(response.data?.styles?.[0]?.style_id ?? null);
       })
       .catch((error) => {
         console.error('Error fetching data: ', error.response || error);
       });
   }, []);
 
+console.log(selectedStyleId)
   return (
     <div>
       <h1>Overview</h1>
-      <StaticStarRating />
+      {/* <StaticStarRating /> */}
       <Information product={product} />
-      {product && <StyleSelector styles={product.styles} />}
+      {product && <StyleSelector styles={product.styles} onStyleSelect={setSelectedStyleId} />}
+      {selectedStyleId && <AddToCart selectedStyleId={selectedStyleId} />}
     </div>
   );
 };
 
 export default Overview;
+
--- client/src/components/overview/StyleSelector.jsx
+++ client/src/components/overview/StyleSelector.jsx
@@ -1,128 +1,34 @@
-// import React, { useState } from 'react';
-// // import './StyleSelector.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-// const StyleSelector = ({ styles }) => {
-//   const [selectedStyleId, setSelectedStyleId] = useState(styles.length > 0 ? styles[0].id : null)
+const StyleSelector = ({ productId }) => {
+  const [styles, setStyles] = useState([]);
+  const [selectedStyle, setSelectedStyle] = useState(null);
 
-//   return (
-//     <div className="style-selector">
-//       {selectedStyleId && <h2>{styles.find((style) => style.id === selectedStyleId).name}</h2>}
-//       <div className="thumbnail-container">
-//         {styles.map((style, index) => (
-//           <div
-//             key={style.id}
-//             className={`thumbnail ${index % 4 === 0 ? 'new-row' : ''}`}
-//             onClick={() => style.id !== selectedStyleId && setSelectedStyleId(style.id)}
-//           >
-//             <img src={style.thumbnailUrl} alt={style.name} />
-//             {style.id === selectedStyleId && <div className="checkmark">&#10003;</div>}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+  useEffect(() => {
+    if (productId) {
+      axios.get(`http://localhost:3000/classes/products/${productId}`)
+        .then((response) => {
+          setStyles(response.data.styles);
+          setSelectedStyle(response.data.styles[0]);
+        })
+        .catch((error) => {
+          console.error('Error fetching style data: ', error.response || error);
+        });
+    }
+  }, [productId]);
 
-// StyleSelector.defaultProps = {
-//   styles: [],
-// };
-
-// export default StyleSelector;
-
-// import React, { useState } from 'react';
-
-// const StyleSelector = ({ styles }) => {
-//   const [selectedStyle, setSelectedStyle] = useState(styles[0]);
-
-//   return (
-//     <div>
-//       <h2>{selectedStyle.name}</h2>
-//       {styles.map((style, index) => (
-//         <div key={style.style_id} style={{ position: 'relative', display: 'inline-block', margin: '10px' }}>
-//           <img
-//             src={style.photos[0].thumbnail_url}
-//             alt={style.name}
-//             onClick={() => setSelectedStyle(style)}
-//             style={{ width: '50px', height: '50px' }}
-//           />
-//           {style.style_id === selectedStyle.style_id &&
-//             <div style={{
-//               position: 'absolute',
-//               top: '0',
-//               left: '0',
-//               width: '50px',
-//               height: '50px',
-//               display: 'flex',
-//               alignItems: 'center',
-//               justifyContent: 'center',
-//               backgroundColor: 'rgba(255, 255, 255, 0.5)',
-//             }}>
-//               ✔️
-//             </div>
-//           }
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default StyleSelector;
-
-// import React, { useState } from 'react';
-
-// const StyleSelector = ({ styles }) => {
-//   const [selectedStyle, setSelectedStyle] = useState(styles[0]);
-
-//   return (
-//     <div>
-//       <h2>{selectedStyle.name}</h2>
-//       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-//         {styles.map((style, index) => (
-//           <div key={style.style_id} style={{
-//             position: 'relative', width: '25%', boxSizing: 'border-box', padding: '5px'
-//           }}>
-//             <img
-//               src={style.photos[0].thumbnail_url}
-//               alt={style.name}
-//               onClick={() => setSelectedStyle(style)}
-//               style={{ width: '100%' }}
-//             />
-//             {style.style_id === selectedStyle.style_id &&
-//               <div style={{
-//                 position: 'absolute',
-//                 top: '0',
-//                 left: '0',
-//                 width: '100%',
-//                 height: '100%',
-//                 display: 'flex',
-//                 alignItems: 'center',
-//                 justifyContent: 'center',
-//                 backgroundColor: 'rgba(255, 255, 255, 0.5)'
-//               }}>
-//                 ✔️
-//               </div>
-//             }
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default StyleSelector;
-
-import React, { useState } from 'react';
-
-const StyleSelector = ({ styles }) => {
-  const [selectedStyle, setSelectedStyle] = useState(styles[0]);
+  if (!selectedStyle) {
+    return null;
+  }
 
   return (
     <div>
       <h2>{selectedStyle.name}</h2>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {styles.map((style, index) => (
+        {styles.map((style) => (
           <div key={style.style_id} style={{
-            position: 'relative', width: '25%', boxSizing: 'border-box', padding: '5px'
+            position: 'relative', width: '25%', boxSizing: 'border-box', padding: '5px',
           }}>
             <img
               src={style.photos[0].thumbnail_url}
