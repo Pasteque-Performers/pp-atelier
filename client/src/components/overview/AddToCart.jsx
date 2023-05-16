db3a7cb3cd422ca8c96f3edffe6fca4e1f891068
Nathaniel Franklin <nmf900@gmail.com>
2023-05-16 07:57:02 -0700

    Add to cart now receiving style object

--- client/src/components/overview/AddToCart.jsx
+++ client/src/components/overview/AddToCart.jsx
@@ -1,61 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// const AddToCart = ({ selectedStyleId }) => {
-//   const [sizes, setSizes] = useState([]);
-//   const [selectedSize, setSelectedSize] = useState('');
-//   const [selectedQuantity, setSelectedQuantity] = useState(1);
-//   const [quantityOptions, setQuantityOptions] = useState([]);
-
-//   useEffect(() => {
-//     axios.get(`classes/products/${selectedStyleId}/styles`)
-//       .then((response) => {
-//         setSizes(response.data.results[0].skus);
-//       })
-//       .catch((error) => {
-//         console.error('Error fetching data: ', error.response || error);
-//       });
-//   }, [selectedStyleId]);
-
-//   useEffect(() => {
-//     const sizeObj = sizes.find((size) => size.size === selectedSize);
-//     if (sizeObj) {
-//       const maxQuantity = Math.min(sizeObj.quantity, 15);
-//       setQuantityOptions(Array.from({ length: maxQuantity }, (_, i) => i + 1));
-//     }
-//   }, [selectedSize, sizes]);
-
-//   const handleSizeChange = (e) => {
-//     setSelectedSize(e.target.value);
-//     setSelectedQuantity(1);
-//   };
-
-//   const handleQuantityChange = (e) => {
-//     setSelectedQuantity(e.target.value);
-//   };
-
-//   const handleAddToCart = () => {
-//     console.log(`Size: ${selectedSize}, Quantity: ${selectedQuantity}`);
-//   };
-
-//   return (
-//     <div>
-//       <select value={selectedSize} onChange={handleSizeChange}>
-//         <option value="">Select Size</option>
-//         {sizes.map((sizeObj) => <option key={sizeObj.size} value={sizeObj.size}>{sizeObj.size}</option>)}
-//       </select>
-
-//       <select value={selectedQuantity} onChange={handleQuantityChange} disabled={!selectedSize}>
-//         {quantityOptions.map((quantity) => <option key={quantity} value={quantity}>{quantity}</option>)}
-//       </select>
-
-//       <button onClick={handleAddToCart} disabled={!selectedSize || !selectedQuantity}>Add to Cart</button>
-//     </div>
-//   );
-// };
-
-// export default AddToCart;
-
 import React, { useState, useEffect } from 'react';
 
 const AddToCart = ({ selectedStyle }) => {
@@ -63,7 +5,7 @@ const AddToCart = ({ selectedStyle }) => {
   const [selectedSize, setSelectedSize] = useState('');
   const [selectedQuantity, setSelectedQuantity] = useState(1);
   const [quantityOptions, setQuantityOptions] = useState([]);
-
+  console.log(selectedStyle);
   useEffect(() => {
     if (selectedStyle && selectedStyle.skus) {
       const availableSizes = Object.values(selectedStyle.skus).map(sku => sku.size);
@@ -92,6 +34,7 @@ const AddToCart = ({ selectedStyle }) => {
   const handleAddToCart = () => {
     console.log(`Size: ${selectedSize}, Quantity: ${selectedQuantity}`);
   };
+  console.log('SelectedStyle in AddToCart: ', selectedStyle);
 
   return (
     <div>
